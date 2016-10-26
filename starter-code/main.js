12a2709ae645ff32fdc17e882b23bb2871d463d6
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-10-25 22:48:46 -0600

    js loop checking for match

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,16 @@ var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+var pickOne;
+var pickTwo;
+
+if((pickOne === cardOne || cardTwo) && (pickTwo === cardTwo || cardOne)){
+alert("BINGO! You found a match!")
+}
+else if((pickOne === cardThree || cardTwo) && (pickTwo === cardThree || cardTwo)){
+alert("BINGO! You found a match!")
+}
+else{
+alert("Sorry, try again!")
+}
