6e5f6adc1be35d9fe16e816933b1db14d61a641e
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-10-27 12:36:20 -0600

    js card flip and match

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,20 +1,60 @@
-console.log("JS file is connected to HTML! Woo!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay=[];
 
 var createCards = function(){
 	var gameBoard = document.getElementById('game-board');
 
-	for (i = 0; i < 4; i++) {
+	for (i = 0; i < cards.length; i++) {
 		var cardDiv = document.createElement('div');
-	cardDiv.className = 'card';
-	gameBoard.appendChild(cardDiv);
-}
+		cardDiv.className = 'card';
+		gameBoard.appendChild(cardDiv);
+		cardElement.addEventListener('click', isTwoCards);
+
+	}
 };
 
-createCards();
+var gameBoard = document.getElementById("game-board");
+
+var createBoard = function(){
+	for (var i = 0; i< cards.length; i++){
+		var cardElement = document.createElement("div");
+		cardElement.className = "card";
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener("click", isTwoCards);
+		gameBoard.appendChild(cardElement);
+	}
+};
+
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute("data-card"));
+	console.log(this.getAttribute("data-card"));
+	if(this.getAttribute('data-card') === 'king'){
+		this.innerHTML = '<img src="king.png" class = "myImgClass" alt="king" />';
+	} 
+	else {
+		this.innerHTML = '<img src="queen.png" class = "myImgClass" alt="queen" />';
+	}
+
+		if (cardsInPlay.length ===2){
+			isMatch(cardsInPlay);
+			cardsInPlay = []; //clears cards
+		}
+};
+
+
+
+var isMatch = function(cards){
+	if(cards[0]===cards[1]){
+		alert ("BINGO!");
+	}
+	else{
+		alert ("Nope. Try again");
+	}
+};
+
+
+
+createBoard(); //displays game
 /*
 var pickOne;
 var pickTwo;
