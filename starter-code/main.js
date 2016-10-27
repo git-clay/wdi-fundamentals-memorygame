c8648ed1a9ecd45edd3e311ca4ddb65ed33ae885
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-10-26 22:00:19 -0600

    js loop

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,21 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
+var createCards = function(){
+	var gameBoard = document.getElementById('game-board');
+
+	for (i = 0; i < 4; i++) {
+		var cardDiv = document.createElement('div');
+	cardDiv.className = 'card';
+	gameBoard.appendChild(cardDiv);
+}
+};
+
+createCards();
+/*
 var pickOne;
 var pickTwo;
 
@@ -16,3 +28,4 @@ alert("BINGO! You found a match!")
 else{
 alert("Sorry, try again!")
 }
+*/
